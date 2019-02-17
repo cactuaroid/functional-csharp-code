567f130d35c2f60e3f68b1ab1093aa39f5438556
cactuaroid <solamiredoredore@gmail.com>
2019-02-17 21:47:30 +0900

    chapter 4

--- Exercises/Chapter04/Exercises.cs
+++ Exercises/Chapter04/Exercises.cs
@@ -5,46 +5,72 @@ using LaYumba.Functional;
 
 namespace Exercises.Chapter4
 {
-   static class Exercises
-   {
-      // 1 Implement Map for ISet<T> and IDictionary<K, T>. (Tip: start by writing down
-      // the signature in arrow notation.)
+    using static LaYumba.Functional.F;
 
-      // 2 Implement Map for Option and IEnumerable in terms of Bind and Return.
+    static class Exercises
+    {
+        // 1 Implement Map for ISet<T> and IDictionary<K, T>. (Tip: start by writing down
+        // the signature in arrow notation.)
 
-      // 3 Use Bind and an Option-returning Lookup function (such as the one we defined
-      // in chapter 3) to implement GetWorkPermit, shown below. 
+        // Map: ISet<T> -> (T -> R) -> ISet<R>
+        public static ISet<R> Map<T, R>(this ISet<T> set, Func<T, R> mapper)
+            => new HashSet<R>(set.Select(mapper));
 
-      // Then enrich the implementation so that `GetWorkPermit`
-      // returns `None` if the work permit has expired.
+        // Map: IDictionary<K, T> -> (T -> R) -> IDictionary<K, R>
+        public static IDictionary<K, R> Map<K, T, R>(this IDictionary<K, T> dictionary, Func<T, R> mapper)
+            => new Dictionary<K, R>(dictionary.Select((x) => new KeyValuePair<K, R>(x.Key, mapper(x.Value))));
 
-      static Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> people, string employeeId)
-      {
-         throw new NotImplementedException();
-      }
+        // 2 Implement Map for Option and IEnumerable in terms of Bind and Return.
+        // Map: Option<T> -> (T -> R) -> Option<R>
+        public static Option<R> Map<T, R>(this Option<T> option, Func<T, R> mapper)
+            => option.Bind((x) => Some(mapper(x)));
 
-      // 4 Use Bind to implement AverageYearsWorkedAtTheCompany, shown below (only
-      // employees who have left should be included).
+        // Map: IEnumerable<T> -> (T -> R) -> IEnumerable<R>
+        public static IEnumerable<R> Map<T, R>(this IEnumerable<T> sequence, Func<T, R> mapper)
+            => sequence.Bind((x) => List(mapper(x)));
 
-      static double AverageYearsWorkedAtTheCompany(List<Employee> employees)
-      {
-         // your implementation here...
-         throw new NotImplementedException();
-      }
-   }
+        // 3 Use Bind and an Option-returning Lookup function (such as the one we defined
+        // in chapter 3) to implement GetWorkPermit, shown below. 
 
-   public struct WorkPermit
-   {
-      public string Number { get; set; }
-      public DateTime Expiry { get; set; }
-   }
+        // Then enrich the implementation so that `GetWorkPermit`
+        // returns `None` if the work permit has expired.
 
-   public class Employee
-   {
-      public string Id { get; set; }
-      public Option<WorkPermit> WorkPermit { get; set; }
+        static Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> people, string employeeId)
+            => people.Lookup(employeeId)
+                .Bind((x) => x.WorkPermit);
 
-      public DateTime JoinedOn { get; }
-      public Option<DateTime> LeftOn { get; }
-   }
+        static Option<WorkPermit> GetWorkPermit2(Dictionary<string, Employee> people, string employeeId)
+            => people.Lookup(employeeId)
+                .Bind((x) => x.WorkPermit)
+                .Where((x) => IsUnexpiredWorkPermit(x, DateTime.Now));
+
+        private static bool IsUnexpiredWorkPermit(WorkPermit workPermit, DateTime now)
+            => workPermit.Expiry >= now;
+
+        // 4 Use Bind to implement AverageYearsWorkedAtTheCompany, shown below (only
+        // employees who have left should be included).
+
+        static double AverageYearsWorkedAtTheCompany(List<Employee> employees)
+            => employees
+                .Bind(GetWorkingDuration)
+                .Average((x) => x.TotalDays / 365);
+
+        private static Option<TimeSpan> GetWorkingDuration(Employee employee)
+            => employee.LeftOn.Map((leftOn) => leftOn - employee.JoinedOn);
+    }
+
+    public struct WorkPermit
+    {
+        public string Number { get; set; }
+        public DateTime Expiry { get; set; }
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+        public Option<WorkPermit> WorkPermit { get; set; }
+
+        public DateTime JoinedOn { get; }
+        public Option<DateTime> LeftOn { get; }
+    }
 }
